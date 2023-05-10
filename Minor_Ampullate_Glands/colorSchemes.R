b409c8fc083361cc828f4fe0b7bb38c8d797666b
Wenqing Chen <wqchen0217@gmail.com>
2023-05-10 15:20:54 +0200

    Add files via upload

---
+++ Minor_Ampullate_Glands/colorSchemes.R
@@ -0,0 +1,156 @@
+
+
+GlandColors <- function(){
+  GlandColors = c(
+    Major = "#87CEFA",
+    Minor = "#FFA07A"
+  )
+}
+
+AmpullateColors <- function(){
+  AmpullateColors = c(
+    Minor_01 = "#FFB6C1",
+    Minor_02 = "#EEA2AD",
+    Minor_03 = "#FFA07A",
+    Minor_04 = "#CD8162",
+    Minor_05 = "#CD5B45",
+    Major_1a = "#BBFFFF",
+    Major_1b = "#96CDCD",
+    Major_02 = "#B0E2FF",
+    Major_03 = "#87CEFA",
+    Major_04 = "#00BFFF",
+    Major_05 = "#CAE1FF",
+    Major_06 = "#BCD2EE",
+    Major_07 = "#8DB6CD"
+  )
+}
+
+AmpullateColors_2 <- function(){
+  AmpullateColors = c(
+    Major_11 = "#104680",
+    Major_12 = "#317db7",
+    Major_13 = "#6198c2",
+    Major_18 = "#6fa7c7",
+    Major_19 = "#8DB6CD",
+    Major_20 = "#6dadd1",
+    Major_7a = "#b6d7e8",
+    Major_7b = "#e9f1f4",
+    Minor_01 = "#fbe3d5",
+    Minor_02 = "#f6b293",
+    Minor_03 = "#dc6d57",
+    Minor_04 = "#b72230",
+    Minor_05 = "#6d0120"
+  )
+}
+
+AmpullateCelltypes <- function(){
+  AmpullateColors = c(
+    Minor_Tail_MiSpabd = "#4995C6",
+    Minor_Sac_MiSpc = "#FABB6E",
+    Minor_Sac_1 = "#FC8002",
+    Minor_Duct_1 = "#C6ABF5",
+    Minor_Duct_2 = "#9968ED",
+    Major_Tail_Spice8 = "#B4DE2CFF",
+    Major_Tail_MaSp1 = "#6DCD59FF",
+    Major_Tail_MaSp2 = "#FDE725FF",
+    Major_Sac_MaSp3 = "#1F9E89FF",
+    Major_Sac_LASC013329 = "#31688EFF",
+    Major_Sac_1 = "#26828EFF",
+    Major_Duct_1 = "#482878FF",
+    Major_Duct_2 = "#440154FF"
+  )
+}
+
+MajorColors <- function(){
+  MajorColors = c(
+    Tail_1 = "#00CCFF",
+    Tail_2 = "#008CFF",
+    Tail_3 = "#004CFF",
+    Sac_B = "#155D27",
+    Sac_3 = "#208B3A",
+    Sac_2 = "#2DC653",
+    Duct_2 = "#DC2F02",
+    Duct_1 = "#F48C06"
+  )
+}
+
+MaspColors <- function(){
+  MaspColors = c(
+    MaSp1 = "#004CFF",
+    MaSp2 = "#00CCFF",
+    MaSp4 = "#008CFF",
+    MaSp3 = "#155D27",
+    Spidroin= "#2DC653",
+    SpiCE = "#F48C06",
+    Other = "#555555"
+  )
+}
+
+######
+MajorColors_2 <-function() {
+  MajorColors_2 = c(
+    Major_0 ="#FDE725FF",
+    Major_1 ="#FDE725FF",
+    Major_7 ="#FDE725FF",
+    Major_4 ="#1F9E89FF",
+    Major_2 ="#1F9E89FF",
+    Major_5 ="#1F9E89FF",
+    Major_3 ="#482878FF",
+    Major_6 ="#482878FF",
+    Major_8 ="#482878FF"
+      )
+} 
+
+MajorColors_3 <-function() {
+  MajorColors_3 = c(
+    Tail ="#1F9E89FF",
+    Sac ="#FDE725FF",
+    Duct = "#482878FF")
+} 
+
+MinorColors <-function() {
+  MinorColors = c(
+    Tail_1 ="#CEDFEF",
+    Tail_2 ="#92C2DD",
+    Tail_3 ="#4995C6",
+    Tail_6 ="#1663A9",
+    Sac_13 ="#FCDCCF",
+    Sac_4 ="#FABB6E",
+    Sac_9 ="#FC8002",
+    Duct_0 ="#C6ABF5",
+    Duct_11 ="#9968ED",
+    Duct_5 ="#8D76B5",
+    unsure = "#addec7"
+  )
+} 
+
+MinorCelltypes <-function() {
+  MinorCelltypes = c(
+    Minor_Tail ="#7fa5f5",
+    Minor_MiSpc ="#4995C6",
+    Minor_Sac ="#FC8002",
+    Minor_Duct ="#9968ED"
+  )
+}
+
+MinorCelltypes_2 <-function() {
+  MinorCelltypes_2 = c(
+    Tail_0 ="#92C2DD",
+    Tail_4 ="#4995C6",
+    Sac_3 ="#FC8002",
+    Sac_5 ="#FABB6E",
+    Duct_1 ="#C6ABF5",
+    Duct_2 ="#9968ED"
+  )
+}
+
+MinorCelltypes_3 <-function() {
+  MinorCelltypes_3 = c(
+    Tail_MiSpabd ="#4995C6",
+    Sac_1 ="#FC8002",
+    Sac_MiSpc ="#FABB6E",
+    Duct_1 ="#C6ABF5",
+    Duct_2 ="#9968ED"
+  )
+}
+
